9affa86fd68836afeb80d03490570794f264441d
mihai <iampenguinsource@gmail.com>
2020-10-22 21:20:09 -0400

    init

---
+++ build/precache-manifest.7b8084077f03c032c095bdb9aba65319.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e133bb16592f549a0362d7094830f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "771355aaf676586cfb3d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f8373a438c6aa6054957",
+    "url": "/static/js/2.14e6cfe8.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.14e6cfe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "771355aaf676586cfb3d",
+    "url": "/static/js/main.ed0de294.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
